3bca4365809edf75e077c45803f91a1158f2b806
Aditya Ashvin <53205379+AdityaAshvin@users.noreply.github.com>
2025-08-03 00:46:16 -0700

    updating day3

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -21,7 +21,7 @@ WHERE NOT City IN (SELECT city
 
 -- b. Without using Subquery
 
-SELECT DISTINCT e.City
+SELECT DISTINCT c.City
 FROM Customers c
 LEFT JOIN Employees e
 ON e.City = c.City
@@ -58,7 +58,7 @@ SELECT c.City, (SELECT SUM (od.Quantity)
 				JOIN Orders o
 				ON o.OrderID = od.OrderID
 				JOIN Customers c1
-				ON c.CustomerID = o.CustomerID
+				ON c1.CustomerID = o.CustomerID
 				WHERE c1.City = c.City
 				) AS TotalProducts
 FROM Customers c
@@ -80,7 +80,7 @@ ON o.CustomerID = c.CustomerID
 JOIN [Order Details] od
 ON od.OrderID = o.OrderID
 GROUP By c.City
-HAVING COUNT (od.ProductID) >= 2
+HAVING COUNT (DISTINCT od.ProductID) >= 2
 ORDER BY c.City
 
 -- 7. List all Customers who have ordered products, but have the ‘ship city’ on the order different from their own customer cities.
@@ -93,15 +93,92 @@ WHERE c.City != o.ShipCity
 
 -- 8. List 5 most popular products, their average price, and the customer city that ordered most quantity of it.
 
-SELECT p.ProductName, COUNT (p.productID) AS OrderCount, AVG (od.UnitPrice * od.Quantity) AS AvgPrice, c.City
-FROM Customers c
-JOIN Orders o
-ON c.CustomerID = o.CustomerID
-JOIN [Order Details] od
-ON od.OrderID = o.OrderID
-JOIN Products p
-ON p.ProductID = od.ProductID
-GROUP By c.City, p.ProductID, p.ProductName
-ORDER BY COUNT (p.productID) DESC
+--a. find the top 5 most popular products and their avg price
 
--- 9. 
+WITH TopProducts AS (
+	SELECT TOP 5 p.ProductID, p.ProductName, SUM (od.Quantity) AS ProductCount, AVG (od.UnitPrice) AS AvgPrice
+	FROM Products p
+	JOIN [Order Details] od
+	ON p.ProductID = od.ProductID
+	GROUP BY p.ProductID, p.ProductName
+	ORDER BY SUM (od.Quantity) DESC
+	),
+
+--b. customer city that ordered most quantity of it
+
+CustomerCity AS (
+	SELECT c.City, p.ProductID, p.ProductName, SUM (od.Quantity) AS MostQuantity, 
+	DENSE_RANK() OVER(PARTITION BY p.ProductID ORDER BY SUM(od.Quantity) DESC) AS rnk
+	FROM Customers c
+	JOIN Orders o
+	ON o.CustomerID = c.CustomerID
+	JOIN [Order Details] od
+	ON od.OrderID = o.OrderID
+	JOIN Products P
+	ON od.ProductID = p.ProductID
+	GROUP BY c.City, p.ProductID, p.ProductName
+	)
+
+SELECT tp.ProductName, tp.AvgPrice, cc.City
+FROM TopProducts tp
+JOIN CustomerCity cc
+ON tp.ProductID = cc.ProductID
+WHERE cc.rnk = 1
+
+-- 9. List all cities that have never ordered something but we have employees there.
+
+-- a. Use sub-query
+
+SELECT e.City
+FROM Employees e
+WHERE e.City NOT IN (
+	SELECT c.City
+	FROM Customers c
+	INNER JOIN Orders o
+	ON o.CustomerID = c.CustomerID
+	)
+
+-- b. Do not use sub-query
+
+SELECT e.City
+FROM Employees e
+LEFT JOIN Customers c
+ON e.City = c.City
+LEFT JOIN Orders o
+ON o.CustomerID = c.CustomerID
+WHERE o.OrderID IS NULL
+
+-- 10. List one city, if exists, that is the city from where the employee sold most orders (not the product quantity) is, and also the city of most total quantity of products ordered from. (tip: join  sub-query)
+
+WITH CityMostOrders AS (
+	SELECT TOP 1 e.EmployeeID, e.City, e.FirstName + ' ' + e.LastName AS Employee, COUNT (od.orderID) AS OrderCount
+	FROM Employees e
+	JOIN Orders o
+	ON o.EmployeeID = e.EmployeeID
+	JOIN [Order Details] od
+	ON od.OrderID = o.OrderID
+	GROUP BY e.EmployeeID, e.FirstName, e.LastName, e.City
+	ORDER BY COUNT (od.orderID) DESC
+),
+CityMostQuantity AS (
+	SELECT TOP 1 e.EmployeeID, e.City, e.FirstName + ' ' + e.LastName AS Employee, SUM (od.Quantity) AS QuantitySold
+	FROM Employees e
+	JOIN Orders o
+	ON o.EmployeeID = e.EmployeeID
+	JOIN [Order Details] od
+	ON od.OrderID = o.OrderID
+	GROUP BY e.EmployeeID, e.FirstName, e.LastName, e.City
+	ORDER BY SUM (od.Quantity) DESC
+)
+
+SELECT cmo.City
+FROM CityMostOrders cmo
+JOIN CityMostQuantity cmq
+ON cmo.City = cmq.City
+
+-- 11. How do you remove the duplicates record of a table?
+
+-- 1 ROW_NUMBER then DELETE
+-- 2. Temp Table
+-- 3. GROUP BY
+-- 4. DELETE in Subquery
